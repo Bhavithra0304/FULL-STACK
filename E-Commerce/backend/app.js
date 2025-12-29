6427b99000f49ee394149bbc9dae3e1dd845d085
Bhavithra0304 <bhavithra.m2024cse@sece.ac.in>
2025-12-29 13:02:47 +0530

    Images to the ecommerce website

--- E-Commerce/backend/app.js
+++ E-Commerce/backend/app.js
@@ -2,13 +2,15 @@ const express=require("express");
 const app=express();
 const dotenv=require("dotenv");
 const path=require("path")
+const cors=require("cors")
 dotenv.config({path:path.join(__dirname,"config","config.env")})
-
+app.use("/images", express.static(path.join(__dirname, "images")));
 const connectDatabase=require("./config/connectDatabase")
 const product=require("./routes/product");
 const order=require("./routes/order")
 
 connectDatabase();
+app.use(cors());
 app.use(express.json())
 app.use("/api/v1",product);
 app.use("/api/v1",order);
